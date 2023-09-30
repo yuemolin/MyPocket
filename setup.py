43fcfb7087d0d8399e1f12af611bd750e2d1ab5a
yuemolin <39358286+yuemolin@users.noreply.github.com>
2023-09-30 14:06:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # Package metadata
 NAME = 'MyPocket'
 DESCRIPTION = 'Collect some interesting computation tools'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 AUTHOR = 'Molin Yue'
 EMAIL = 'yuemolin@umich.edu'
 URL = 'https://github.com/yuemolin/ToolBox'
